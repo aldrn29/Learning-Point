23f2f3a26cabf0329e5bea260dbdc867240021b3
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2020-06-05 18:05:13 +0900

    Update main.js

--- Webpage Step01/main.js
+++ Webpage Step01/main.js
@@ -1,31 +1,27 @@
 'use strict';
 
-// On this codelab, you will be streaming only video (video: true).
+//getUserMedia() 호출시 어떤 트랙이 포함되는지 지정
 const mediaStreamConstraints = {
   video: true,
 };
-/*
-MediaStreamConstraints dictionary
-반환 시 어떤 종류의 트랙이 포함되어야 하는지 지정하기 위해 
-getUserMedia()를 호출할 때 사용된다. 
-*/
 
-// 스트림이 위치할 로컬비디오
-const localVideo = document.querySelector('video');
-/*
-Document의 메서드 querySelector(DOMString selsector)는 선택된 selector 
-또는 selsector 그룹과 일치하는 문서 내 '첫 번째 Element'를 반환한다. 
-일치하는 요소가 없으면 null을 반환한다.
-*/
 
-// Local stream that will be reproduced on the video.
+// 스트림이 위치할 로컬비디오 html에서 param으로 접근한다.
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+
 let localStream;
-//let은 블록 유효 범위를 갖는 지역 변수이다.
+let remoteStream;
+
+let localPeerConnection;
+let remotePeerConnection;
 
 // 비디오 요소에 미디어스트림을 추가하는 function
 function gotLocalMediaStream(mediaStream) {
   localStream = mediaStream; //param 값을 localStream에도,
-  localVideo.srcObject = mediaStream;//
+  localVideo.srcObject = mediaStream;// 순서?
+  trace('Received local stream.')
+  startButton.disabled = false; //start버튼 누를 수 있게
 }
 
 // 예외처리 에러 로그 메시지
@@ -33,12 +29,64 @@ function handleLocalMediaStreamError(error) {
   console.log('navigator.getUserMedia error: ', error);
 }
 
-// 버튼 정의
+//상대방 비디오를 가져올때
+function gotRemoteMediaStream(event) {
+  const mediaStream = event.stream; //이부분에서 상대 peer의 화면을 가져와야 하는거 아닌가?
+  remoteVideo.srcObject = mediaStream;
+}
+
+// RTC peer connection 을 정의
+
+// 새로운 peer 연결 new peer candidate로
+function handleConnection(event) {
+  const peerConnection = event.target;
+  const iceCandidate = event.candidate;
+
+  if(iceCandidate){
+    const newIceCandidate = new RTCIceCandidate(iceCandidate);
+    const otherPeer = getOtherPeer(peerConnection);
+
+    otherPeer.addIceCandidate(newIceCandidate);
+
+    // .then(() => {
+    //   handleConnectionSuccess(peerConnection);
+    // }).catch((error) => {
+    //   handleConnectionFailure(peerConnection, error);
+    // });
+
+  }
+}
+
+// // Logs that the connection succeeded.
+// function handleConnectionSuccess(peerConnection) {
+//   trace(`${getPeerName(peerConnection)} addIceCandidate success.`);
+// };
+
+// // Logs that the connection failed.
+// function handleConnectionFailure(peerConnection, error) {
+//   trace(`${getPeerName(peerConnection)} failed to add ICE Candidate:\n`+
+//         `${error.toString()}.`);
+// }
+
+// // Logs changes to the connection state.
+// function handleConnectionChange(event) {
+//   const peerConnection = event.target;
+//   console.log('ICE state change event: ', event);
+//   trace(`${getPeerName(peerConnection)} ICE state: ` +
+//         `${peerConnection.iceConnectionState}.`);
+// }
+
+// 버튼 정의와 초기화
 const startButton = document.getElementById('startButton');
+const callButton = document.getElementById('callButton');
+const hangupButton = document.getElementById('hangupButton');
+callButton.disabled = true;
+callButton.disabled = true;
 
 // 미디어 스트림 초기화
 function startAction() {
   startButton.disabled = true;
+  // navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
   navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
     .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
   trace('Requesting local stream.');
@@ -51,6 +99,15 @@ function startAction() {
   성공하면 위에 정의한 function gotLocalMediaStream을 통해 MediaStream 이 반환된다.
   이는 srcObject속성을 통해 미디어 요소에서 사용할 수 있다.
   */
+
+function callAction() {
+  callButton.disabled = true;
+  hangupButton.disabled = false;
+
+  trace('Start calling');
+  
+  const videoTracks = remoteStream.getVideoTracks();
+}
 
  // 버튼 클릭 이벤트 핸들러
  startButton.addEventListener('click', startAction);
